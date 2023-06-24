8ad040524f32e5ca4b51deba3ad81064582edb41
Manoel Vieira <netinhovieira1801@gmail.com>
2023-06-24 13:40:52 -0300

    Atualizando usuário

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,5 +7,6 @@ const usersRoutes = Router() // Inicializando o Router das rotas do usuário
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create)
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
